9d3dd44ff07f2672625e103ee9867e8cfc74ffd0
Vishal Singh <115693661+vishal-2023@users.noreply.github.com>
2024-06-25 22:54:27 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,53 +1,39 @@
-const data = [
-	{
-		id: 1,
-		name: "Agra",
-		info: "Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
-		image: "https://cdn.thecodehelp.in/Agra.jpeg",
-		price: "35,758",
-	},
-	{
-		id: 2,
-		name: "Jaipur",
-		info: `Jaipur is the capital city of the Indian state of Rajasthan, known for its stunning pink-hued buildings and palaces. The city is also known as the "Pink City" due to the color of many of its buildings. Jaipur is famous for its intricate textiles, colorful handicrafts, and delicious cuisine.`,
-		image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
-		price: "82,560",
-	},
-	{
-		id: 3,
-		name: "Goa",
-		info: " Goa is a small state on India's western coast, known for its stunning beaches, vibrant nightlife, and Portuguese-influenced architecture. The state is also known for its spicy seafood dishes and unique cultural traditions.",
-		image: "https://cdn.thecodehelp.in/Goa.jpeg",
-		price: "29,695",
-	},
-	{
-		id: 4,
-		name: "Varanasi",
-		info: "Varanasi is a holy city in northern India, situated on the banks of the sacred river Ganges. The city is a major pilgrimage site for Hindus and is famous for its ancient temples, winding alleyways, and colorful festivals.",
-		image: "https://cdn.thecodehelp.in/Varanasi.jpeg",
-		price: "31,095",
-	},
-	{
-		id: 5,
-		name: "Darjeeling",
-		info: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!",
-		image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
-		price: "78,595",
-	},
-	{
-		id: 6,
-		name: "Jaisalmer",
-		info: "Jaisalmer is a city in the Indian state of Rajasthan, known for its stunning sandstone architecture and its location in the heart of the Thar Desert. The city is home to several impressive palaces and fortresses, as well as unique cultural traditions and handicrafts.",
-		image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
-		price: "68,595",
-	},
-	{
-		id: 7,
-		name: "Kochi",
-		info: "Kochi is a port city in the Indian state of Kerala, known for its historic connections to trade and commerce. The city is home to several stunning churches, synagogues, and mosques, as well as a rich culinary tradition that blends Indian, Portuguese, and other influences.",
-		image: "https://cdn.thecodehelp.in/Kochi.jpeg",
-		price: "68,595",
-	},
+const reviews = [
+  {
+    id: 1,
+    name: "Pranay Gupta",
+    job: "Software Engineer",
+    image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Abir Pal",
+    job: "Graphic Designer",
+    image: "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Soumya Banerjee",
+    job: "Marketing Manager",
+    image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Saikat Mukherjee",
+    job: "Content Strategist",
+    image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Aritra Biswas",
+    job: "Data Analyst",
+    image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
 ];
 
-export default data;
+export default reviews;
